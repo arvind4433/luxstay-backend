162271f4bd08d0ae219f044896a1ecdd9fa21ebe
Arvind Singh <arvind889481@gmail.com>
2025-11-24 15:22:11 +0530

    message

--- services/mail.js
+++ services/mail.js
@@ -1,10 +1,10 @@
 const nodemailer = require("nodemailer");
 
-// Create a test account or replace with real credentials.
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 2525,
+  secure: false,
   auth: {
     user: "9aefde001@smtp-brevo.com",
     pass: process.env.BREVO_API_KEY,
@@ -17,7 +17,7 @@ const SendMail = async (tomail, subject, message) => {
     from: '"Pulse" <arvind8802225@gmail.com>',
     to: tomail,
     subject: subject,
-       html: message, // HTML body
+       html: message, 
   });
 
   console.log("Message sent:", info.messageId);
